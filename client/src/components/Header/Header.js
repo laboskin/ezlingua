c409a75b8f952abcf9cd5465a158b0bc682fcc67
Artem Laboskin <laboss@list.ru>
2020-09-13 23:14:55 +0300

    Refactored header styles according to BEM

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -30,102 +30,102 @@ function Header() {
     ];
     const userIsAdmin = true;
     return (
-        <header>
-            <div className="header-container">
-                <a className="header-logo" href="/">
+        <header className="Header">
+            <div className="Header-Container">
+                <a className="Header-Logo" href="/">
                     <img className="" src={logo} alt=""/>
                 </a>
-                <div className="header-nav">
-                    <NavLink className="header-nav-button" to="/content">Content</NavLink>
-                    <NavLink className="header-nav-button" to="/dictionary">Dictionary</NavLink>
-                    <NavLink className="header-nav-button" to="/training">Training</NavLink>
+                <div className="Header-Nav">
+                    <NavLink className="Header-NavButton" to="/content">Content</NavLink>
+                    <NavLink className="Header-NavButton" to="/dictionary">Dictionary</NavLink>
+                    <NavLink className="Header-NavButton" to="/training">Training</NavLink>
                 </div>
-                <div className="header-user">
-                    <div className="header-user-language">
+                <div className="Header-User">
+                    <div className="Header-Language">
                         <img className=""
                              src={currentCourse.image}
                              alt=""/>
-                            <div className="language-popup">
-                                <div className="language-popup-container">
-                                    <div className="language-popup-item language-popup-item-current">
-                                        <div className="language-popup-item-icon">
+                            <div className="Header-LanguagePopup">
+                                <div className="Header-LanguagePopupContainer">
+                                    <div className="Header-LanguagePopupItem Header-LanguagePopupItem_current">
+                                        <div className="Header-LanguagePopupItemImage">
                                             <img className=""
                                                  src={currentCourse.image}
                                                  alt=""/>
                                         </div>
-                                        <span className="language-popup-item-text">
+                                        <span className="Header-LanguagePopupItemText">
                                             {currentCourse.name}
                                         </span>
                                     </div>
-                                    <div className="language-popup-delimeter"/>
+                                    <div className="Header-LanguagePopupDelimeter"/>
                                     { otherCourses.map((course) => {
                                         return (
-                                            <Link className="language-popup-item language-popup-item-new"
+                                            <Link className="Header-LanguagePopupItem Header-LanguagePopupItem_new"
                                                      to="/user/change-course">
-                                                <div className="language-popup-item-icon">
+                                                <div className="Header-LanguagePopupItemImage">
                                                     <img className=""
                                                          src={course.image}
                                                          alt=""/>
                                                 </div>
-                                                <span className="language-popup-item-text">
+                                                <span className="Header-LanguagePopupItemText">
                                                 {course.name}
                                             </span>
                                             </Link>
                                         )}
                                     )}
-                                    <Link className="language-popup-item language-popup-item-add"
+                                    <Link className="Header-LanguagePopupItem Header-LanguagePopupItem_add"
                                        to="/user/add-course">
-                                        <div className="language-popup-item-icon">
+                                        <div className="Header-LanguagePopupItemIcon">
                                             <ReactSVG src={plusIcon}/>
                                         </div>
-                                        <span className="language-popup-item-text">
+                                        <span className="Header-LanguagePopupItemText">
                                             Add
                                         </span>
                                     </Link>
                                 </div>
-                                <div className="language-popup-triangle">
-                                    <div className="language-popup-diamond"/>
+                                <div className="Header-LanguagePopupTriangle">
+                                    <div className="Header-LanguagePopupDiamond"/>
                                 </div>
                             </div>
                     </div>
 
-                    <div className="header-user-profile">
+                    <div className="Header-Profile">
                         <img className="" src={userAvatar} alt=""/>
-                            <div className="profile-popup">
-                                <div className="profile-popup-container">
+                            <div className="Header-ProfilePopup">
+                                <div className="Header-ProfilePopupContainer">
                                     { userIsAdmin && (
-                                        <Link className="profile-popup-item"
+                                        <Link className="Header-ProfilePopupItem"
                                                  to="/admin">
-                                            <div className="profile-popup-item-icon">
+                                            <div className="Header-ProfilePopupItemIcon">
                                                 <ReactSVG src={adminIcon}/>
                                             </div>
-                                            <span className="profile-popup-item-text">
+                                            <span className="Header-ProfilePopupItemText">
                                                 Admin panel
                                             </span>
                                         </Link>
                                     )
                                     }
-                                    <Link className="profile-popup-item"
+                                    <Link className="Header-ProfilePopupItem"
                                              to="user/settings">
-                                        <div className="profile-popup-item-icon">
+                                        <div className="Header-ProfilePopupItemIcon">
                                             <ReactSVG src={settingsIcon}/>
                                         </div>
-                                        <span className="profile-popup-item-text">
+                                        <span className="Header-ProfilePopupItemText">
                                             Settings
                                         </span>
                                     </Link>
-                                    <Link className="profile-popup-item"
+                                    <Link className="Header-ProfilePopupItem"
                                        to="/user/logout">
-                                        <div className="profile-popup-item-icon">
+                                        <div className="Header-ProfilePopupItemIcon">
                                             <ReactSVG src={logoutIcon}/>
                                         </div>
-                                        <span className="profile-popup-item-text">
+                                        <span className="Header-ProfilePopupItemText">
                                             Logout
                                         </span>
                                     </Link>
                                 </div>
-                                <div className="profile-popup-triangle">
-                                    <div className="profile-popup-diamond"/>
+                                <div className="Header-ProfilePopupTriangle">
+                                    <div className="Header-ProfilePopupDiamond"/>
                                 </div>
                             </div>
                     </div>
