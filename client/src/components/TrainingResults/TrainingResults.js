9fcbf7b9037673ffe776f79adb7f8c0b77a5037e
Artem Laboskin <laboss@list.ru>
2020-09-18 00:02:49 +0300

    Refactored TrainingResult component according to BEM

--- client/src/components/TrainingResults/TrainingResults.js
+++ client/src/components/TrainingResults/TrainingResults.js
@@ -1,63 +1,54 @@
 import React from 'react';
 import {Link, useParams} from "react-router-dom";
 import './style.scss';
-import MainContainer from "../../hoc/MainContainer/MainContainer";
 import IconArrowRight from "../../icons/IconArrowRight/IconArrowRight";
 
 function TrainingResults(props) {
     const {id: vocabularyId = ''} = useParams();
 
     return (
-        <MainContainer maxWidth="700px">
-            <div className="page-title">
-                <div className="page-name">
-                    <div className="page-name-text">{props.title}</div>
+        <div className="TrainingResult">
+            <div className="TrainingResult-Title">
+                <div className="TrainingResult-Name">
+                    {props.title}
                 </div>
             </div>
-            <div className="training-card">
-                <div className="training-word">
-                    <div className="training-word-text">
-                        Results
-                    </div>
-                </div>
-                <div className="training-results">
-                    {
-                        props.words.map(word => (
-                            <div className={`training-result ${word.correct?'correct':'wrong'}`}>
-                                <div className="training-result-text">
-                                <span className="training-result-word">
+            <div className="TrainingResult-Card">
+                Results
+            </div>
+            <div className="TrainingResult-Words">
+                {
+                    props.words.map(word => (
+                        <div className={`TrainingResult-Word ${word.correct?'TrainingResult-Word_correct':'TrainingResult-Word_wrong'}`}>
+                            <span className="TrainingResult-WordOriginal">
                                     {word.original}
                                 </span>
-                                    <span className="training-result-dash">
+                            <span className="TrainingResult-WordDash">
                                     —
                                 </span>
-                                    <span className="training-result-translation">
+                            <span className="TrainingResult-WordTranslation">
                                     {word.translation}
-                                </span>
-                                </div>
-                            </div>
-                        ))
-                    }
-                </div>
-                <div className="training-buttons">
-                    <Link className="training-button exit"
-                          to={`/training/${vocabularyId}`}>
-                        <div className="training-button-text exit">
-                            Exit
+                            </span>
                         </div>
-                    </Link>
-                    <div className="training-button continue"
-                    onClick={props.handleReload}>
-                        <div className="training-button-text continue">
-                            Continue
-                        </div>
-                        <div className="training-button-icon continue">
-                            <IconArrowRight />
-                        </div>
+                    ))
+                }
+            </div>
+            <div className="TrainingResult-Buttons">
+                <Link className="TrainingResult-Button TrainingResult-Button_exit"
+                      to={`/training/${vocabularyId}`}>
+                    <div className="TrainingResult-ButtonText">
+                        Exit
                     </div>
+                </Link>
+                <div className="TrainingResult-Button TrainingResult-Button_continue"
+                     onClick={props.handleReload}>
+                    <div className="TrainingResult-ButtonText">
+                        Continue
+                    </div>
+                    <IconArrowRight />
                 </div>
             </div>
-        </MainContainer>
+        </div>
 
     )
 }
