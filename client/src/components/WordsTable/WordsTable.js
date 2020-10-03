9d5024e9c87ae572cc0f467fa68cf4f68e030665
Artem Laboskin <laboss@list.ru>
2020-10-03 16:02:33 +0300

    Refactored WordsTable component

--- client/src/components/WordsTable/WordsTable.js
+++ client/src/components/WordsTable/WordsTable.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import './style.scss';
 import {Link} from "react-router-dom";
-import IconProgressLowHorizontal from "../../icons/progress/IconProgressLowHorizontal/IconProgressLowHorizontal";
-import IconProgressMiddleHorizontal from "../../icons/progress/IconProgressMiddleHorizontal/IconProgressMiddleHorizontal";
 import IconProgressHighHorizontal from "../../icons/progress/IconProgressHighHorizontal/IconProgressHighHorizontal";
 import IconTrash from "../../icons/IconTrash/IconTrash";
 import IconSpeaker from "../../icons/IconSpeaker/IconSpeaker";
+import IconProgressLowHorizontal from "../../icons/progress/IconProgressLowHorizontal/IconProgressLowHorizontal";
+import IconProgressMiddleHorizontal
+    from "../../icons/progress/IconProgressMiddleHorizontal/IconProgressMiddleHorizontal";
+import {learnWordFromVocabulary, removeWord} from "../../store/actions/dictionary";
+import {useDispatch} from "react-redux";
+import IconPlus from "../../icons/IconPlus/IconPlus";
 
 function WordsTable(props) {
-
+    const dispatch = useDispatch();
     return (
         <div className="words-table">
             {
                 props.words.map(word => (
-                    <div className="words-row">
+                    <div className="words-row" key={word.id}>
                         <div className="word-audio">
                             <IconSpeaker />
                         </div>
@@ -25,18 +29,33 @@ function WordsTable(props) {
                                 {word.translation}
                             </div>
                         </div>
-                        <Link className="word-vocabulary"
-                              to={`/dictionary/my/${word.vocabularyId}`}>
-                            {word.vocabularyName}
-                        </Link>
-                        <div className="word-progress">
-                            <div className="word-progress-icon">
-                                <IconProgressHighHorizontal />
+                        {
+                            word.vocabulary && (
+                                <Link className="word-vocabulary"
+                                      to={`/dictionary/my/${word.vocabulary.id}`}>
+                                    {word.vocabulary.name}
+                                </Link>
+                            )
+                        }
+                        {!props.vocabularyId && (
+                            <React.Fragment>
+                                <div className="word-progress">
+                                    <div className="word-progress-icon">
+                                        { word.isNew && <IconProgressLowHorizontal /> }
+                                        { word.isLearning && <IconProgressMiddleHorizontal /> }
+                                        { word.isLearned && <IconProgressHighHorizontal /> }
+                                    </div>
+                                </div>
+                                <div className="word-actions-delete" onClick={() => dispatch(removeWord(word.id))}>
+                                    <IconTrash />
+                                </div>
+                            </React.Fragment>
+                            )}
+                        {(props.vocabularyId && !word.userWords) && (
+                            <div className="word-actions-add" onClick={() => dispatch(learnWordFromVocabulary(word.id, props.vocabularyId))}>
+                                <IconPlus />
                             </div>
-                        </div>
-                        <div className="word-actions-delete">
-                            <IconTrash />
-                        </div>
+                        )}
                     </div>
                 ))
             }
