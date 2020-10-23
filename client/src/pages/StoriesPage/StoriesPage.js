266429927a894cc76ff6540bd7f0304d2484720d
Artem Laboskin <laboss@list.ru>
2020-10-23 23:55:57 +0300

    Added logic to StoriesPage component

--- client/src/pages/StoriesPage/StoriesPage.js
+++ client/src/pages/StoriesPage/StoriesPage.js
@@ -1,6 +1,6 @@
-import React, {useCallback, useEffect} from 'react';
+import React, {useCallback, useEffect, useRef} from 'react';
 import {useDispatch, useSelector} from "react-redux";
-import {clearStory, loadStory, selectWord, unselectWord} from "../../store/actions/stories";
+import {addWord, clearStory, loadStory, selectWord, unselectWord} from "../../store/actions/stories";
 import MainContainer from "../../hoc/MainContainer/MainContainer";
 import './style.scss';
 import {useParams} from "react-router-dom";
@@ -11,10 +11,11 @@ function StoriesPage() {
     const { id: storyId } = useParams();
     const currentCourse = useSelector(state => state.user.courses.currentCourse);
     const story = useSelector(state => state.stories.story);
+    const addedWords = useSelector(state => state.stories.addedWords);
     const selectedWord = useSelector(state => state.stories.selectedWord);
+    const selectedWordRef = useRef();
     const translations = useSelector(state => state.stories.translations);
-    const translationsVisible = !!translations;
-    const addedWords = [];
+    const isPopoverVisible = !!translations;
 
     useEffect(() => {
         dispatch(loadStory(storyId));
@@ -22,20 +23,20 @@ function StoriesPage() {
     }, [dispatch, currentCourse, storyId]);
 
     const bodyClearTranslationsClickListener = useCallback((event) => {
-        if(!event.target.classList.contains('StoriesPage-TextSpan_selected'))
+        if(!event.target.classList.contains('StoriesPage-TextSpan_selected') && !event.target.closest('.StoriesPage-Popover'))
             dispatch(unselectWord());
     }, [dispatch]);
     useEffect(() => {
-        if (translationsVisible) {
-            document.body.addEventListener('click', bodyClearTranslationsClickListener);
+        if (isPopoverVisible) {
+            document.body.addEventListener('click', bodyClearTranslationsClickListener, false);
+            return () => document.body.removeEventListener('click', bodyClearTranslationsClickListener);
         }
-        return () => document.body.removeEventListener('click', bodyClearTranslationsClickListener);
-    }, [dispatch, bodyClearTranslationsClickListener, translationsVisible]);
+    }, [dispatch, bodyClearTranslationsClickListener, isPopoverVisible]);
 
     if (!story) return null;
+
     return (
         <MainContainer maxWidth="1100px">
-
             <div className="StoriesPage">
                 <div className="StoriesPage-Main">
                     <div className="StoriesPage-Title">
@@ -46,21 +47,73 @@ function StoriesPage() {
                             if (sentence.isEmpty)
                                 return <br key={sentence.position} />;
                             return sentence.parts.map(part => {
+                                const isAdded = !!addedWords.find(addedWord => addedWord.original === part.text.toLowerCase());
+                                const isSelected = sentence.position === selectedWord.sentencePosition && part.position === selectedWord.partPosition;
+                                const ref = isSelected?selectedWordRef:undefined;
+                                const popoverCssClass = ['StoriesPage-Popover'];
+                                if (isSelected && isPopoverVisible) {
+                                    const isLeftBorder = selectedWordRef.current
+                                        && (selectedWordRef.current.offsetLeft - selectedWordRef.current.offsetWidth < 100);
+                                    const isRightBorder = selectedWordRef.current
+                                        && (selectedWordRef.current.parentElement.offsetWidth - selectedWordRef.current.offsetLeft < 100);
+                                    if(isLeftBorder)
+                                        popoverCssClass.push('StoriesPage-Popover_left');
+                                    else if (isRightBorder)
+                                        popoverCssClass.push('StoriesPage-Popover_right');
+                                }
+
                                 const cssClass = ['StoriesPage-TextSpan'];
                                 if (part.hasTranslation)
                                     cssClass.push('StoriesPage-TextSpan_hasTranslation');
-
-                                if (addedWords.find(selectedWord => selectedWord.original === part.text))
-                                    cssClass.push('StoriesPage-TextSpan_added');
-                                else if (sentence.position === selectedWord.sentencePosition && part.position === selectedWord.partPosition)
+                                if (isSelected)
                                     cssClass.push('StoriesPage-TextSpan_selected');
+                                else if (isAdded)
+                                    cssClass.push('StoriesPage-TextSpan_added');
                                 return (
-                                    <React.Fragment key={sentence.position + '' + part.position}>
-                                    <span className={cssClass.join(' ')} onClick={() => part.hasTranslation && dispatch(selectWord(sentence.position, part.position))}>
+                                    <React.Fragment key={sentence.position + '_' + part.position}>
+                                    <span className={cssClass.join(' ')}
+                                          onClick={() => {
+                                              if (part.hasTranslation)
+                                                  dispatch(selectWord(sentence.position, part.position))
+                                          }}
+                                          ref={ref}>
                                         {part.text}
+                                        {isSelected && isPopoverVisible && (
+                                            <div className={popoverCssClass.join(' ')}>
+                                                <div className="StoriesPage-PopoverDiamond"/>
+                                                <div className="StoriesPage-PopoverTranslations">
+                                                    {translations.map(translation => {
+                                                        const isTranslationAdded = !!addedWords.find(addedWord => addedWord.original === part.text.toLowerCase()
+                                                            && addedWord.translation === translation.text)
+                                                        return (
+                                                            <div key={translation.text}
+                                                                 className={`StoriesPage-PopoverTranslation ${isTranslationAdded?'StoriesPage-PopoverTranslation_added':''}`}
+                                                                 onClick={(event) => {
+                                                                     event.stopPropagation();
+                                                                     dispatch(unselectWord());
+                                                                     if(!isTranslationAdded) {
+                                                                         dispatch(addWord(part.text.toLowerCase(), translation.text));
+                                                                     }
+                                                                 }}>
+                                                                <div className="StoriesPage-PopoverTranslationConfidence">
+                                                                    <div className="StoriesPage-PopoverTranslationConfidenceFilled"
+                                                                         style={{width: `${translation.confidence * 100}%`}}/>
+                                                                </div>
+                                                                <div className="StoriesPage-PopoverTranslationText">
+                                                                    {translation.text}
+                                                                </div >
+                                                                <div className="StoriesPage-PopoverTranslationDelete">
+                                                                    {isTranslationAdded && <IconTrash />}
+                                                                </div>
+                                                            </div>
+                                                        )})}
+                                                </div>
+                                            </div>
+                                        )}
                                     </span>
                                         {part.spaceAfter?' ':''}
-                                    </React.Fragment>)
+                                    </React.Fragment>
+                                )
                             });
                         })}
                     </div>
@@ -69,34 +122,19 @@ function StoriesPage() {
                             Complete
                         </div>
                     </div>
-
-                    <div className="StoriesPage-Popover">
-                        <div className="StoriesPage-PopoverDiamond"/>
-                        <div className="StoriesPage-PopoverTranslations">
-                            <div className="StoriesPage-PopoverTranslation">
-                                <div className="StoriesPage-PopoverTranslationConfidence">
-                                    <div className="StoriesPage-PopoverTranslationConfidenceFilled" />
-                                </div>
-                                <div className="StoriesPage-PopoverTranslationText">
-                                </div>
-                                <div className="StoriesPage-PopoverTranslationDelete">
-                                    <IconTrash />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
                 </div>
 
                 <div className="StoriesPage-Words">
                     <div className="StoriesPage-WordsTitle">
                         Words
                     </div>
-                    <div className="StoriesPage-WordsItem">
-                        <strong>original</strong>
-                        <span>—</span>
-                        <span>translation</span>
-                    </div>
+                    {addedWords.map(word => (
+                        <div className="StoriesPage-WordsItem" key={`${word.original}_${word.translation}`}>
+                            <strong>{word.original}</strong>
+                            <span> — </span>
+                            <span>{word.translation}</span>
+                        </div>
+                    ))}
                 </div>
             </div>
         </MainContainer>
