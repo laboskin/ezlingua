a78acb7d6fcc17e93cfb31f555f9fa36465aa915
Artem Laboskin <laboss@list.ru>
2020-10-27 00:40:17 +0300

    Added translation locales for i18n

---
+++ client/src/i18n/locales/en.js
@@ -0,0 +1,156 @@
+export default {
+    homepage: {
+        websiteLanguage: 'Website language',
+        start: 'Start',
+        signIn: 'Sign in',
+        signInToAccount: 'Sign in to account',
+        banner: {
+            title: 'Learn foreign languages online.',
+            description: 'Easy, efficient, free.'
+        },
+        features: {
+            bestWay: {
+                title: 'The best way to learn foreign languages',
+                description: 'Forget about boring textbooks, thick dictionaries and trips to tutors. Learn foreign languages online with ezlingua! Anywhere in the world, from any device and at any time convenient for you.'
+            },
+            fromQuotes: {
+                title: 'From quotes to literary masterpieces',
+                description: 'Here you will find many texts in foreign languages, by studying which you can not only learn new words, but also learn a lot for yourself. The site contains materials of varying complexity - suitable for both beginners and polyglot.'
+            },
+            getReady: {
+                title: 'Get ready for an interview or an international travel',
+                description: 'No more hours of searching for translations of words and copying them into a notebook. Using thematic sets of foreign words, you can prepare for an important event - from going to a restaurant to an important interview. Choose the appropriate set of words and add the necessary words to your personal dictionary.'
+            },
+            speedUp: {
+                title: 'Speed up your progress',
+                description: 'Learn foreign words with training. A wide selection of vocabulary workouts is available on the site - from classic vocabulary cards to word constructor. Take training, track progress and improve your language skills.'
+            }
+        }
+    },
+    header: {
+        stories: 'Stories',
+        dictionary: 'Dictionary',
+        training: 'Training',
+        add: 'Add',
+        admin: 'Admin Panel',
+        settings: 'Settings',
+        logout: 'Logout'
+    },
+    storiesIndex: {
+        stories: 'Stories'
+    },
+    storiesPage: {
+        words: 'Words',
+        complete: 'Complete'
+    },
+    trainingNames: {
+        cards: 'Cards',
+        constructor: 'Constructor',
+        listening: 'Listening',
+        translationWord: 'Translation-word',
+        wordTranslation: 'Word-translation'
+    },
+    trainingIndex: {
+        training: 'Training',
+        allVocabularies: 'All vocabularies',
+        pluralWords: '{count, plural, ' +
+            '=0{Not enough words}' +
+            '=1{# word}' +
+            'other{# words}}',
+    },
+    trainingQuiz: {
+        iKnow: 'I know',
+        iDontKnow: 'I don\'t know',
+        repeat: 'Repeat',
+        onRepeat:'On repeat',
+        skip: 'skip',
+        exit: 'Exit',
+        next: 'Next'
+    },
+    trainingResults: {
+        results: 'Results',
+        exit: 'Exit',
+        continue: 'Continue'
+    },
+    dictionaryIndex: {
+        myVocabularies: 'My vocabularies',
+    },
+    dictionaryProgress: {
+        myDictionary: 'My dictionary',
+        show: 'Show',
+        new: 'New',
+        learning: 'Learning',
+        learned: 'Learned',
+        training: 'Training',
+        pluralWords: '{count, plural, ' +
+            '=1{# word}' +
+            'other{# words}}',
+    },
+    vocabularyCard: {
+        learn: 'Learn',
+        remove: 'Remove',
+        pluralWords: '{count, plural, ' +
+            '=1{# word}' +
+            'other{# words}}',
+    },
+    dictionaryView: {
+        vocabulary: 'Vocabulary',
+        pluralWords: '{count, plural, ' +
+            '=1{# word}' +
+            'other{# words}}',
+    },
+    dictionaryMy: {
+        myVocabulary: 'My vocabulary',
+        pluralWords: '{count, plural, ' +
+            '=1{# word}' +
+            'other{# words}}',
+    },
+    vocabularyTitle: {
+        myVocabulary: 'My vocabulary',
+        learnVocabulary: 'Learn vocabulary',
+        pluralWords: 'Show all {count, plural, ' +
+            '=1{# word}' +
+            'other{# words}}',
+    },
+    wordsFilters: {
+        search: 'Search',
+        allTrainings: 'All trainings',
+        all: 'All',
+        training: 'Training'
+    },
+    loginForm: {
+        loginToYourAccount: 'Login to your account',
+        email: 'Email',
+        password: 'Password',
+        login: 'Login',
+        signUp: 'Sign up',
+        errors: {
+            emailIsRequired: 'Email is required',
+            validEmail: 'Please enter valid email',
+            passwordMore: 'Password length must be more than',
+            passwordLess: 'Password length must be less than',
+            passwordForbiddenCharacters: 'Password contains forbidden characters'
+        }
+    },
+    registerForm: {
+        createNewAccount: 'Create new account',
+        language: 'Language',
+        name: 'Name',
+        email: 'Email',
+        password: 'Password',
+        signUp: 'Sign up',
+        signIn: 'Sign in',
+        errors: {
+            emailIsRequired: 'Email is required',
+            validEmail: 'Please enter valid email',
+            nameMore: 'Password length must be more than',
+            nameLess: 'Password length must be less than',
+            passwordMore: 'Password length must be more than',
+            passwordLess: 'Password length must be less than',
+            passwordForbiddenCharacters: 'Password contains forbidden characters',
+        }
+    },
+    courseSelectForm: {
+        availableCourses: 'Available courses'
+    }
+}
---
+++ client/src/i18n/locales/ru.js
@@ -0,0 +1,174 @@
+export default {
+    homepage: {
+        websiteLanguage: 'Язык сайта',
+        start: 'Начать',
+        signIn: 'Войти',
+        signInToAccount: 'Войти в аккаунт',
+        banner: {
+            title: 'Изучайте иностранные языки онлайн.',
+            description: 'Удобно, эффективно, бесплатно.'
+        },
+        features: {
+            bestWay: {
+                title: 'Лучший способ изучения иностранных языков',
+                description: 'Забудьте про скучные учебники, толстые словари и походы к репетиторам. Изучайте иностранные языки онлайн с ezlingua! В любой точке мира, с любого устройства и в любое удобное для вас время.'
+            },
+            fromQuotes: {
+                title: 'От цитат до литературных шедевров',
+                description: 'Здесь вы найдете множество текстов на иностранных языках, изучая которые вы не только сможете узнать новые слова, но и узнать для себя много нового. На сайте представлены материалы различной сложности - подойдет как новичкам, так и полиглотам.'
+            },
+            getReady: {
+                title: 'Подготовьтесь к собеседованию или международному путешествию',
+                description: 'Больше не нужно часами искать переводы слов и переписывать их в тетрадь. При помощи тематических наборов иностранных слов вы сможете подготовиться к важному событию - от похода в ресторан до важного собеседования. Выберите подходящий набор слов и добавьте необходимые слова в свой личный словарь.'
+            },
+            speedUp: {
+                title: 'Ускорьте свой прогресс',
+                description: 'Изучайте иностранные слова при помощи тренировок. На сайте доступен широкий выбор словарный тренировок - от классических словарных карточек до конструктора слов. Проходите тренировки, отслеживайте прогресс и повышайте свой уровень знания языка.'
+            }
+        }
+    },
+    header: {
+        stories: 'Истории',
+        dictionary: 'Словарь',
+        training: 'Тренировки',
+        add: 'Добавить',
+        admin: 'Админ панель',
+        settings: 'Настройки',
+        logout: 'Выход'
+    },
+    storiesIndex: {
+        stories: 'Истории'
+    },
+    storiesPage: {
+        words: 'Слова',
+        complete: 'Завершить'
+    },
+    trainingNames: {
+        cards: 'Карточки',
+        constructor: 'Конструктор',
+        listening: 'Аудирование',
+        translationWord: 'Перевод-слово',
+        wordTranslation: 'Слово-перевод'
+    },
+    trainingIndex: {
+        training: 'Тренировки',
+        allVocabularies: 'Все наборы слов',
+        pluralWords: '{count, plural, ' +
+            '=0{Недостаточно слов}' +
+            '=1{# слово}' +
+            'one{# слово}' +
+            'few{# слова}' +
+            'many{# слов}' +
+            'other{# слов}}',
+    },
+    trainingQuiz: {
+        iKnow: 'Знаю',
+        iDontKnow: 'Не знаю',
+        repeat: 'Повторить',
+        onRepeat:'На повторении',
+        skip: 'пропустить',
+        exit: 'Завершить',
+        next: 'Далее'
+    },
+    trainingResults: {
+        results: 'Результаты',
+        exit: 'Завершить',
+        continue: 'Продолжить'
+    },
+    dictionaryIndex: {
+        myVocabularies: 'Мои наборы слов',
+    },
+    dictionaryProgress: {
+        myDictionary: 'Мой словарь',
+        show: 'Показать',
+        new: 'Новые',
+        learning: 'Изучаю',
+        learned: 'Знаю',
+        training: 'Тренировки',
+        pluralWords: '{count, plural, ' +
+            '=1{# слово}' +
+            'one{# слово}' +
+            'few{# слова}' +
+            'many{# слов}' +
+            'other{# слов}}',
+    },
+    vocabularyCard: {
+        learn: 'Изучить',
+        remove: 'Удалить',
+        pluralWords: '{count, plural, ' +
+            '=1{# слово}' +
+            'one{# слово}' +
+            'few{# слова}' +
+            'many{# слов}' +
+            'other{# слов}}',
+    },
+    dictionaryView: {
+        vocabulary: 'Набор слов',
+        pluralWords: '{count, plural, ' +
+            '=1{# слово}' +
+            'one{# слово}' +
+            'few{# слова}' +
+            'many{# слов}' +
+            'other{# слов}}',
+    },
+    dictionaryMy: {
+        myVocabulary: 'Мой словарь',
+        pluralWords: '{count, plural, ' +
+            '=1{# слово}' +
+            'one{# слово}' +
+            'few{# слова}' +
+            'many{# слов}' +
+            'other{# слов}}',
+    },
+    vocabularyTitle: {
+        myVocabulary: 'Мой набор',
+        learnVocabulary: 'Изучить набор',
+        pluralWords: 'Показать все {count, plural, ' +
+            '=1{# слово}' +
+            'one{# слово}' +
+            'few{# слова}' +
+            'many{# слов}' +
+            'other{# слов}}',
+    },
+    wordsFilters: {
+        search: 'Поиск',
+        allTrainings: 'Все тренировки',
+        all: 'Все',
+        training: 'Тренировки'
+    },
+    loginForm: {
+        loginToYourAccount: 'Вход',
+        email: 'Электронная почта',
+        password: 'Пароль',
+        login: 'Войти',
+        signUp: 'Регистрация',
+        errors: {
+            emailIsRequired: 'Введите электронную почту',
+            validEmail: 'Введите корректный адрес',
+            passwordMore: 'Длина пароля должна быть больше чем',
+            passwordLess: 'Длина пароля должна быть меньше чем',
+            passwordForbiddenCharacters: 'Пароль содержит запрещенные символы'
+        }
+    },
+    registerForm: {
+        createNewAccount: 'Создать новый аккаунт',
+        language: 'Язык',
+        name: 'Имя',
+        email: 'Электронная почта',
+        password: 'Пароль',
+        signUp: 'Создать',
+        signIn: 'Вход',
+        errors: {
+            emailIsRequired: 'Введите электронную почту',
+            validEmail: 'Введите корректный адрес',
+            nameMore: 'Длина имени должна быть больше чем',
+            nameLess: 'Длина имени должна быть меньше чем',
+            passwordMore: 'Длина пароля должна быть больше чем',
+            passwordLess: 'Длина пароля должна быть меньше чем',
+            passwordForbiddenCharacters: 'Пароль содержит запрещенные символы'
+        }
+    },
+    courseSelectForm: {
+        availableCourses: 'Доступные курсы'
+    }
+}
