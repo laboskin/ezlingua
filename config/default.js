bca042d8a27d71e4d6d453b120d0a3742d5e5297
Artem Laboskin <laboss@list.ru>
2020-10-30 20:19:33 +0300

    Added example of server config with placeholders

---
+++ config/default.js
@@ -0,0 +1,13 @@
+module.exports = {
+    port : 5000,
+    mongoUri :"MONGODB_CONNECTION_URI",
+    jwtConfig: {
+        secret: "JWT_SECRET",
+        accessTokenAge: 60*60,
+        refreshTokenAge: 30*24*60*60*1000
+    },
+    azure: {
+        endpoint: 'AZURE_TRANSLATOR_ENDPOINT',
+        key: 'AZURE_TRANSLATOR_KEY'
+    }
+}
