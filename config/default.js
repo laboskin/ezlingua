1c4e73306689e0be69589a22aac53e2ee5781568
Artem Laboskin <laboss@list.ru>
2020-11-26 10:59:39 +0300

    Prepared config for deployment

--- config/default.js
+++ config/default.js
@@ -1,14 +1,14 @@
 module.exports = {
-    port : 5000,
-    mongoUri :"MONGODB_CONNECTION_URI",
+    port : process.env.PORT || 5000,
+    mongoUri : process.env.MONGODB_CONNECTION_URI || "MONGODB_CONNECTION_URI",
     jwtConfig: {
-        secret: "JWT_SECRET",
-        adminSecret: "JWT_ADMIN_SECRET",
+        secret: process.env.JWT_SECRET || "JWT_SECRET",
+        adminSecret: process.env.JWT_ADMIN_SECRET || "JWT_ADMIN_SECRET",
         accessTokenAge: 60*60,
         refreshTokenAge: 30*24*60*60*1000
     },
     azure: {
-        endpoint: 'AZURE_TRANSLATOR_ENDPOINT',
-        key: 'AZURE_TRANSLATOR_KEY'
+        endpoint: process.env.AZURE_TRANSLATOR_ENDPOINT || 'AZURE_TRANSLATOR_ENDPOINT',
+        key: process.env.AZURE_TRANSLATOR_KEY || 'AZURE_TRANSLATOR_KEY'
     }
 }
